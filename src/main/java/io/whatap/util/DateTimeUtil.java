2dd949ae44d664718fa12e839f81594c872356db
BEDongryeol <ehdfuf53@gmail.com>
2023-03-07 14:21:35 +0900

    [FEAT] LocalDateTime <-> String, Long 변환 Util 추가

---
+++ src/main/java/io/whatap/util/DateTimeUtil.java
@@ -0,0 +1,22 @@
+package io.whatap.util;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Copyright whatap Inc since 2023/03/07
+ * Created by Larry on 2023/03/07
+ * Email : inwoo.server@gmail.com
+ */
+public class DateTimeUtil {
+    private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    public static String toString(LocalDateTime dateTime) {
+        return dateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN));
+    }
+
+    public static Long toLong(LocalDateTime dateTime) {
+        return dateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+}
