8912f237ca96083ee985a83ea691ec62405b104a
BEDongryeol <ehdfuf53@gmail.com>
2023-03-08 21:42:23 +0900

    [FEAT] RequestLogRepository에 FileRepository 주입
    - 파일을 조회하는 책임을 fileRepository에 위임

--- src/main/java/io/whatap/repository/RequestLogRepository.java
+++ src/main/java/io/whatap/repository/RequestLogRepository.java
@@ -3,14 +3,11 @@ package io.whatap.repository;
 import io.whatap.data.RequestLogPack;
 import io.whatap.io.DataReader;
 import io.whatap.io.DataWriter;
-import io.whatap.io.FileReader;
 import io.whatap.io.FileWriter;
-import io.whatap.io.support.FileLoader;
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 /**
  * Copyright whatap Inc since 2023/03/06
@@ -20,8 +17,15 @@ import java.nio.file.Files;
 @Repository
 public class RequestLogRepository {
 
+    private final FileRepository fileRepository;
+
+    public RequestLogRepository(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
+
     public Boolean save(String fileName, RequestLogPack log) {
-        File file = FileLoader.loadByFileName(fileName);
+        File file = fileRepository.loadFileByName(fileName);
+
         DataWriter dataWriter = DataWriter.typeOfByteArray();
         log.write(dataWriter);
 
@@ -29,11 +33,9 @@ public class RequestLogRepository {
         return file.exists();
     }
 
-    public RequestLogPack read(String requestFileName) throws IOException {
-        File file = FileLoader.loadByFileName(requestFileName);
-
-        byte[] bytes = FileReader.readAll(Files.newInputStream(file.toPath()));
+    public RequestLogPack read(byte[] bytes) throws IOException {
         DataReader dataReader = DataReader.typeOfByteArray(bytes);
         return RequestLogPack.create(dataReader);
     }
+
 }
