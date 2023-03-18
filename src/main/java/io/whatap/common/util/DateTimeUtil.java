1f1d0ae537568a0908bd9bde815252c7308e0f4b
BEDongryeol <ehdfuf53@gmail.com>
2023-03-19 01:25:03 +0900

    [FEAT] 현재 시간을 long으로 return 메소드 추가

--- src/main/java/io/whatap/common/util/DateTimeUtil.java
+++ src/main/java/io/whatap/common/util/DateTimeUtil.java
@@ -1,8 +1,11 @@
 package io.whatap.common.util;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.function.Supplier;
 
 /**
  * Copyright whatap Inc since 2023/03/07
@@ -18,5 +21,18 @@ public class DateTimeUtil {
 
     public static Long toLong(LocalDateTime dateTime) {
         return dateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
+    public static LocalDateTime toLocalDateTime(long epochTime) {
+        return Instant.ofEpochMilli(epochTime)
+                .atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    public static Long getNowUnixTime() {
+        return toLong(currentTimeSupplier().get());
+    }
+
+    private static Supplier<LocalDateTime> currentTimeSupplier() {
+        return LocalDateTime::now;
     }
 }
