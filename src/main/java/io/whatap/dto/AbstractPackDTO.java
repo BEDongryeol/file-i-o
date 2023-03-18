de028855a01ea3271f3fb80d7bfcd32b9101c7e4
BEDongryeol <ehdfuf53@gmail.com>
2023-03-19 01:30:44 +0900

    [FEAT] DTO validation

--- src/main/java/io/whatap/dto/AbstractPackDTO.java
+++ src/main/java/io/whatap/dto/AbstractPackDTO.java
@@ -2,9 +2,14 @@ package io.whatap.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+
 @Data
 public abstract class AbstractPackDTO {
+
+    @Min(value = 1, message = "프로젝트 번호는 1 이상이어야 합니다.")
     private long projectCode; // 프로젝트 번호
+    @Min(value = 1, message = "Agent Id는 1 이상이어야 합니다.")
     private int agentId; // 에이전트 아이디
     private long time; // 로그가 생성된 시간
 
